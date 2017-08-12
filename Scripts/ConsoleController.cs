a282e3b078317c7e6a811c8e1614f509cc27c791
Andrey Khudozhidkov <khudo.dev@gmail.com>
2017-08-12 18:48:40 +0500

    first

---
+++ Scripts/ConsoleController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace ConsoleLog
+{
+    public class ConsoleController : MonoBehaviour
+    {
+        public static ConsoleController Instance;
+
+        [SerializeField] public Text text;
+        [SerializeField] public InputField inputField;
+
+        private bool isShow;
+
+        private bool IsShow
+        {
+            get { return isShow; }
+            set
+            {
+                isShow = value;
+                GetComponent<Animator>().SetBool("show", isShow);
+                if (isShow)
+                {
+                    inputField.ActivateInputField();
+                }
+                else
+                {
+                    inputField.DeactivateInputField();
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            text.text = "";
+            inputField.onEndEdit.AddListener(OnEndEditCommand);
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void LogMessage(string message)
+        {
+            text.text += message + "\n";
+        }
+
+        private void Update()
+        {
+            if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) &&
+                (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) &&
+                Input.GetKeyDown(KeyCode.C))
+            {
+                IsShow = !IsShow;
+            }
+            else if (IsShow && Input.GetKeyDown(KeyCode.Escape))
+            {
+                IsShow = false;
+            }
+        }
+
+        void OnEndEditCommand(string text)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Log.ConsoleMessage("Run command: " + text);
+                inputField.text = "";
+                inputField.ActivateInputField();
+            }
+        }
+    }
+}
---
+++ Scripts/ConsoleLog.cs
@@ -0,0 +1,112 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace ConsoleLog
+{
+    public enum LogType
+    {
+        Debug,
+        Info,
+        Warning,
+        Error
+    }
+
+    public class Log
+    {
+        protected static string GetLogPrefix(LogType logType)
+        {
+            return "[" + logType + "]";
+        }
+
+        protected static Color GetLogColor(LogType logType)
+        {
+            switch (logType)
+            {
+                case LogType.Warning:
+                    return Color.yellow;
+                case LogType.Error:
+                    return Color.red;
+                default:
+                    return Color.white;
+            }
+        }
+
+        public static void ConsoleMessage(string message)
+        {
+            CommonOutput(null, LogType.Info, message);
+        }
+
+        public static void Info(string message)
+        {
+            CommonOutput(new StackTrace(true).GetFrame(1), LogType.Info, message);
+        }
+
+        public static void Debug(string message)
+        {
+            CommonOutput(new StackTrace(true).GetFrame(1), LogType.Debug, message);
+        }
+
+        public static void Warning(string message)
+        {
+            CommonOutput(new StackTrace(true).GetFrame(1), LogType.Warning, message);
+        }
+
+        public static void Error(string message)
+        {
+            CommonOutput(new StackTrace(true).GetFrame(1), LogType.Error, message);
+        }
+
+        protected static void CommonOutput(StackFrame frame, LogType logType, string message)
+        {
+            ConsoleOutput(frame, GetLogPrefix(logType), GetLogColor(logType), message);
+            UnityLogOutput(logType, GetLogString(frame, GetLogPrefix(logType), message));
+        }
+
+        protected static void UnityLogOutput(LogType logType, string message)
+        {
+            switch (logType)
+            {
+                case LogType.Warning:
+                    UnityEngine.Debug.LogWarning(message);
+                    break;
+                case LogType.Error:
+                    UnityEngine.Debug.LogError(message);
+                    break;
+                default:
+                    UnityEngine.Debug.Log(message);
+                    break;
+            }
+        }
+
+        protected static void ConsoleOutput(StackFrame frame, string prefix, Color color, string message)
+        {
+            if (ConsoleController.Instance != null)
+            {
+                string s = "<color=#" + ColorUtility.ToHtmlStringRGBA(color) + ">" +
+                           GetLogString(frame, prefix, message) + "</color>";
+                ConsoleController.Instance.LogMessage(s);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("Console controller is NULL");
+            }
+        }
+
+        protected static string GetLogString(StackFrame frame, string prefix, string message)
+        {
+            if (frame != null)
+            {
+                string[] s = frame.GetFileName().Split('/');
+                string fileName = s[s.Length - 1];
+                fileName = fileName.Remove(fileName.Length - 3, 3);
+
+                return "[" + Time.frameCount + "] " + prefix + " " +
+                       fileName + "." + frame.GetMethod().Name + " (" + frame.GetFileLineNumber() + "): " + message;
+            }
+            else
+            {
+                return prefix + ": " + message;
+            }
+        }
+    }
+}
